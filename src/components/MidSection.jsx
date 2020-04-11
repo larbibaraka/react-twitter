065477567b56c0ba7c1f74697cba82fc27d93026
Baraka Larbi <laridev@gmail.com>
2020-04-11 19:16:54 +0100

    adding tom mid section

--- src/components/MidSection.jsx
+++ src/components/MidSection.jsx
@@ -4,10 +4,14 @@ export default function MidSection() {
   return (
     <div className="w-1/3 border border-grey-300">
       <div className="user_lit_info flex">
-        <span className="border-white h-10 w-10 flex items-center justify-center rounded-full hover:bg-blue-200 ">
-          &lt;
+        <span className="border-white h-10 w-10 flex items-center justify-center rounded-full hover:bg-blue-200 cursor-pointer text-blue-300 text-2xl hover:text-blue-400 ml-2 mr-2 mt-2">
+        &#8592;
         </span>
-        <h1 className="text-2xl font-bold">Baraka Larbi</h1>
+        <div className="flex flex-col">
+
+        <h1 className="text-2xl font-bold mt-2">Baraka Larbi</h1>
+        <span className="text-gray-600 text-sm">180 Tweets</span>
+        </div>
       </div>
     </div>
   );
