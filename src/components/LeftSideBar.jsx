0ee17a83f540c0e34b6ee4f000edc2151ddeae39
Baraka Larbi <laridev@gmail.com>
2020-04-11 18:49:02 +0100

    adding padding

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -4,14 +4,14 @@ export default function LeftSideBar() {
     return (
     
             <ul className="flex flex-col">
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Acceil</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Explore</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Notification</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Messages</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Signets</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Listes</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Profile</a></li>
-                <li className=""><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Plus</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Acceil</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Explore</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Notification</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Messages</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Signets</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Listes</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Profile</a></li>
+                <li className="py-3"><a className="text-center block border rounded-full hover:border-red-600  py-2" href="/">Plus</a></li>
             </ul>    
    
     )
