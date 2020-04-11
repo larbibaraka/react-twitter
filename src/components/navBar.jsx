e3dd083839781b6f933f25b2a15ebf34fe937f7e
Baraka Larbi <laridev@gmail.com>
2020-04-11 18:28:22 +0100

    adding there main components

---
+++ src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto flex items-center py-4">
+        somethnlnlnlqsdnln
+      </div>
+    </div>
+  );
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import LeftSideBar from './../components/LeftSideBar';
+import MidSection from "../components/MidSection";
+import RightSideBar from "../components/RightSideBar";
+ 
 function App() {
   return (
-    <div className="bg-gray-400">
-      <h1 className="text-6l">hello world</h1>
+    <div className="container mx-auto">
+        <LeftSideBar/>
+        <MidSection/>
+        <RightSideBar/>
     </div>
   );
 }
