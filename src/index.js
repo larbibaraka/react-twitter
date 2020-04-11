5df2ddcf6c439a0390453ff6ac742fa9de5a6af8
Baraka Larbi <laridev@gmail.com>
2020-04-11 17:50:54 +0100

    build tailwind & create first component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/tailwind.css";
+import "./assets/main.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
