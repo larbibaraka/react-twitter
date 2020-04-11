ee54f0a79ef2227d057af9f6c81457c23c59c012
Baraka Larbi <laridev@gmail.com>
2020-04-11 18:31:39 +0100

    adding main left navigation

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function LeftSideBar() {
     return (
-        <div>
-            
+        <div className="">
+            <ul className="flex">
+                <li>Acceil</li>
+                <li>Explore</li>
+                <li>Notification</li>
+                <li>Messages</li>
+                <li>Signets</li>
+                <li>Listes</li>
+                <li>Profile</li>
+                <li>Plus</li>
+            </ul>    
         </div>
     )
 }
