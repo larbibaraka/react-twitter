0667a8ab67d83415caf96f3324621daf5c80ea17
Baraka Larbi <laridev@gmail.com>
2020-04-17 16:14:22 +0100

    fix rounded for images right

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 import baseimage from "../assets/images/baseimage.jfif";
 
-
 export default function RightSideBar() {
   return (
     <div className="w-1/3 border border-gray-200 flex flex-col">
+      {/* search bar */}
       <div className="w-full  pl-10 pr-10 mt-5 ">
         <input
           class="rounded-full py-2 px-4 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -14,12 +14,12 @@ export default function RightSideBar() {
           placeholder="Search Twitter"
         />
       </div>
-
-      <div className="h-48 w-full flex flex-col p-4">
+      {/* some user's images */}
+      <div className="w-full flex flex-col p-4">
        <div className="flex mt-1">
        <div className="mr-1">
           <img
-            className="h-24 rounded-l border-l-1 border-t-1 border-b-1  border-gray-500"
+            className="h-24 rounded-l-md border-l-1 border-t-1 border-b-1  border-gray-500"
             src={baseimage}
             alt="Baraka Larbi user background"
           />
@@ -35,7 +35,7 @@ export default function RightSideBar() {
       
         <div className="">
           <img
-            className="h-24 rounded-r  border-l-1 border-t-1 border-b-1 border-gray-500"
+            className="h-24 rounded-r-md  border-l-1 border-t-1 border-b-1 border-gray-500"
             src={baseimage}
             alt="Baraka Larbi user background"
           />
@@ -44,7 +44,7 @@ export default function RightSideBar() {
        <div className="flex mt-1">
        <div className="mr-1">
           <img
-            className="h-24 rounded-l border-l-1 border-t-1 border-b-1  border-gray-500"
+            className="h-24 rounded-l-md border-l-1 border-t-1 border-b-1  border-gray-500"
             src={baseimage}
             alt="Baraka Larbi user background"
           />
@@ -60,14 +60,15 @@ export default function RightSideBar() {
       
         <div className="">
           <img
-            className="h-24 rounded-r  border-l-1 border-t-1 border-b-1 border-gray-500"
+            className="h-24 rounded-r-md  border-l-1 border-t-1 border-b-1 border-gray-500"
             src={baseimage}
             alt="Baraka Larbi user background"
           />
         </div>
        </div>
       </div>
-   
+
+
     </div>
   );
 }
